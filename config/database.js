1f122d2728edcb8cfd5c5d3abfc7f05be48f1b17
Micah-Ella Estacio <micahellae@gmail.com>
2019-04-25 16:35:28 -0400

    config change

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://mdatabase:mdb@cluster0-ij9ve.mongodb.net/test?retryWrites=true', 
+    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'final'
+};
